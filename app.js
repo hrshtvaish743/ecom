9b64bce91d7cb3450f4a186a6c3114d04e52dd3b
Harshit <hrshtvaish743@gmail.com>
2016-09-18 19:54:42 +0530

    Initial Configuration

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session  = require('express-session');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
+
+/*var configDB = require('./config/database.js');
+mongoose.connect(configDB.url, function(err) {
+    if (err) {
+      console.log("Can't connect to DB!");
+      throw err;
+    }
+});*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,14 +26,21 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'windswept',
+  resave: true,
+  saveUninitialized: true
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', routes);
-app.use('/users', users);
+require('./config/passport')(passport);
+require('./routes/routes.js')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +56,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.json('error');
+    res.json('error1');
   });
 }
 
@@ -47,7 +64,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.json('error');
+  res.json('error1');
 });
 
 
